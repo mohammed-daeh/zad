6055f2098a0cc1598e887090809f91a05e4a2425
mohammed-daeh <163894409+mohammed-daeh@users.noreply.github.com>
2025-03-27 03:14:30 +0300

    first commit

--- lib/featuers/auth/presentation/screens/log_in_screen.dart
+++ lib/featuers/auth/presentation/screens/log_in_screen.dart
@@ -4,10 +4,10 @@ import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:zad_almuslem/core/app_colors/app_colors.dart';
 import 'package:zad_almuslem/core/translations/translation.dart';
+import 'package:zad_almuslem/featuers/auth/application/auth_state_provider.dart';
 import 'package:zad_almuslem/featuers/auth/presentation/controllers/auth_controller.dart';
 import 'package:zad_almuslem/shear/widget/text_form_filde_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 class LoginScreen extends ConsumerWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -71,7 +71,7 @@ class LoginScreen extends ConsumerWidget {
                   icon: Icons.email,
                   controller: _emailController,
                 ),
-                if (emailError != null)
+                if (emailError != null && emailError.isNotEmpty)
                   Padding(
                     padding: const EdgeInsets.only(top: 4.0),
                     child: Text(
@@ -161,6 +161,160 @@ class LoginScreen extends ConsumerWidget {
     );
   }
 }
+
+// class LoginScreen extends ConsumerWidget {
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+
+//   LoginScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     // قراءة البروفايدرات الخاصة بحالة التحميل والأخطاء
+//     final isLoading = ref.watch(isLoadingProvider);
+//     final emailError = ref.watch(emailErrorProvider);
+
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       body: Stack(clipBehavior: Clip.none, children: [
+//         Positioned.fill(
+//           child: Container(
+//             decoration: BoxDecoration(
+//               gradient: LinearGradient(
+//                 begin: Alignment.topLeft,
+//                 end: Alignment.bottomRight,
+//                 colors: [
+//                   AppColors.primary, // لون أساسي
+//                   AppColors.secondary, // لون ثانوي
+//                 ],
+//               ),
+//             ),
+//             child: SvgPicture.asset(
+//               'assets/images/splash_screen.svg',
+//             ),
+//           ),
+//         ),
+//         Positioned(
+//           top: 0,
+//           bottom: 0,
+//           right: 0,
+//           left: 0,
+//           child: SingleChildScrollView(
+//             padding: EdgeInsets.all(20),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Gap(60),
+//                 SvgPicture.asset(
+//                   'assets/logo/logo.svg',
+//                   width: 100,
+//                   height: 100,
+//                 ),
+//                 Gap(40),
+//                 Text(
+//                   "Sign In".i18n,
+//                   style: TextStyle(
+//                     fontSize: 32,
+//                     fontWeight: FontWeight.bold,
+//                     color: Colors.white,
+//                   ),
+//                 ),
+//                 Gap(60),
+//                 TextFormFildeAuth(
+//                   hintText: "Email".i18n,
+//                   icon: Icons.email,
+//                   controller: _emailController,
+//                 ),
+//                 if (emailError != null)
+//                   Padding(
+//                     padding: const EdgeInsets.only(top: 4.0),
+//                     child: Text(
+//                       emailError,
+//                       style: TextStyle(color: Colors.red),
+//                     ),
+//                   ),
+//                 Gap(20),
+//                 TextFormFildeAuth(
+//                   hintText: "Password".i18n,
+//                   icon: Icons.lock,
+//                   isPassword: true,
+//                   controller: _passwordController,
+//                 ),
+//                 Gap(5),
+//                 Align(
+//                   alignment: Alignment.centerRight,
+//                   child: TextButton(
+//                     onPressed: () {
+//                       // هنا يمكنك إضافة منطق استعادة كلمة المرور
+//                     },
+//                     child: Text(
+//                       "Forgot Password?".i18n,
+//                       style: TextStyle(
+//                         color: AppColors.yallowLight,
+//                         fontSize: 14,
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 Gap(50),
+//                 ElevatedButton(
+//                   onPressed: isLoading
+//                       ? null
+//                       : () {
+//                           ref.read(authControllerProvider.notifier).signInWithEmail(
+//                                 email: _emailController.text.trim(),
+//                                 password: _passwordController.text.trim(),
+//                               );
+//                         },
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: Colors.white,
+//                     foregroundColor: Color(0xFF2575fc),
+//                     padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(30),
+//                     ),
+//                     elevation: 5,
+//                   ),
+//                   child: isLoading
+//                       ? CircularProgressIndicator(
+//                           color: Colors.blue,
+//                         )
+//                       : Text(
+//                           "Sign In".i18n,
+//                           style: TextStyle(fontSize: 18),
+//                         ),
+//                 ),
+//                 Gap(20),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Text(
+//                       "Don't have an account? ".i18n,
+//                       style: TextStyle(color: Colors.white, fontSize: 14),
+//                     ),
+//                     InkWell(
+//                       onTap: () {
+//                         context.go('/register');
+//                       },
+//                       child: Text(
+//                         "Create an account".i18n,
+//                         style: TextStyle(
+//                           color: AppColors.yallowLight,
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 16,
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 )
+//               ],
+//             ),
+//           ),
+//         )
+//       ]),
+//     );
+//   }
+// }
 // class LoginScreen extends StatelessWidget {
 //   final TextEditingController _emailController = TextEditingController();
 //   final TextEditingController _passwordController = TextEditingController();
